4fa47f658a7f9f182dbf9a4726564a2e8eb920c6
CristianAtienza27 <cristianatienza26@gmail.com>
2023-05-04 15:24:07 +0200

    init commit

--- src/main/java/microservices/springbooteurekaserver/SpringbootEurekaServerApplication.java
+++ src/main/java/microservices/springbooteurekaserver/SpringbootEurekaServerApplication.java
@@ -2,12 +2,14 @@ package microservices.springbooteurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringbootEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEurekaServerApplication.class, args);
+    }
 
 }
